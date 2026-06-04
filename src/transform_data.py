90570617ff90d4293697e42c8606fc05f950721d
Jothan <jothanalmeida7@gmail.com>
2026-06-04 15:52:50 -0300

    feat: adiciona função que converte colunas de datas de str para datetime no arquivo src/transform_data.py. Função de criar copia foi alterada para criar dataframe sem baixar outro csv

--- src/transform_data.py
+++ src/transform_data.py
@@ -4,18 +4,40 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-path_file = Path(__file__).parent.parent/'data'/'hospital_patients_real_world.csv'
+path_file_raw = Path(__file__).parent.parent/'data'/'hospital_patients_real_world.csv'
 
-def copy_data():
+columns_names_to_datetime = ['AdmissionDate', 'DischargeDate']
+columns_names_to_int = ['Age']
+
+def create_dataframe(path_name: str):
     logging.info("Criando copia dos dados para transformação...")
 
-    df = pd.read_csv(path_file)
+    if not path_name.exists():
+        raise FileNotFoundError(f"Arquivo não encontrado: {path_name}\n")
 
-    df.to_csv(
-        path_file.parent / "data_hospital_records.csv",
-        index = False
-    )
+    df = pd.read_csv(path_name)
 
+    logging.info(f"Data Frame criado com {len(df)} linha(s)...")
+
+    return df
+
+def exchange_type_datetime(df: pd.DataFrame, columns_names: list[str])->pd.DataFrame:
+
+    logging.info(f"Convertendo tipos de dados das colunas {columns_names} para datetime...")
+
+    # Converte campos
+    for name in columns_names:
+        df[name] = pd.to_datetime(df[name], format = "%m/%d/%y")
+        #Se der erro no de cima, usar o de baixo
+        #dados['nova_data'] = pd.to_datetime(dados['data'], infer_datetime_format=True)
+
+    logging.info("Colunas convertidas para datetime...")
+
+    return df
+
+
+
+    
 
 
 
