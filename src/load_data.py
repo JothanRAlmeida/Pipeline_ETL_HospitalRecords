146d5ac59de7b4b49cf12068ddd2b435ca6ba402
Jothan <jothanalmeida7@gmail.com>
2026-06-04 14:33:52 -0300

    feat: adiciona a função de download do dataset no arquivo src/load_data.py e baixa o arquivo na pasta data. Adiciona a análise exploratória do dataset no notebook notebooks/analysis_data.ipynb

---
+++ src/load_data.py
@@ -0,0 +1,24 @@
+from kaggle.api.kaggle_api_extended import KaggleApi
+import pandas as pd
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def extract_hospital_data():
+
+    logging.info("Extraindo os dados do dataset...")
+
+    output_dir = Path('data')
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    api = KaggleApi()
+    api.authenticate()
+
+    api.dataset_download_files(
+        "nudratabbas/hospital-records-for-data-cleaning-medium",
+        output_dir,
+        unzip=True
+    )
+
+    logging.info(f"Dados baixados em {output_dir}...")
