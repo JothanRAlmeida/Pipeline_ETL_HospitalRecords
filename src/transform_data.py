620d7f92aa616e0f767f8e7f53c7b9587655c641
Jothan <jothanalmeida7@gmail.com>
2026-06-05 10:47:55 -0300

    feat: adiciona função que cria nova coluna indicando se o periodo de estadia do paciente é valida ou não (DischargeDate >= AdmissionDate)

--- src/transform_data.py
+++ src/transform_data.py
@@ -84,6 +84,16 @@ def fill_nan_median(df: pd.DataFrame, columns_names: list[str])->pd.DataFrame:
 
     return df
 
+def define_valid_stay(df: pd.DataFrame)->pd.DataFrame:
+
+    logging.info("Criando nova coluna para indicar se estadia é válida (DischargeDate >= AdmissionDate)...")
+
+    df['is_valid_stay'] = (df['AdmissionDate'] > df['DischargeDate'])
+
+    logging.info(f"{len(df['is_valid_stay'])} registros com estádia inválida...")
+
+    return df
+
 def transform_data_hospital():
 
 
