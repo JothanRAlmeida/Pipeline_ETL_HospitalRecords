1d54f81032dc9ebc15c35d13ff24f7e1127a950e
Jothan <jothanalmeida7@gmail.com>
2026-06-08 17:48:53 -0300

    docs: adiciona comentários detalhando as etapas de extração

--- src/extract_data.py
+++ src/extract_data.py
@@ -5,20 +5,22 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# Utiliza a API do Kaggle para extrair arquivo com os dados
 def extract_hospital_data():
 
     logging.info("Extraindo os dados do dataset...")
 
-    output_dir = Path('data/raw')
-    output_dir.mkdir(parents=True, exist_ok=True)
+    output_dir = Path('data/raw') # Diretório onde irá salvar o dataset (.csv)
+    output_dir.mkdir(parents=True, exist_ok=True) # Se o caminho não existir, cria as pastas
 
-    api = KaggleApi()
-    api.authenticate()
+    api = KaggleApi() # Instância da classe KaggleApi
+    api.authenticate() # Carrega as credenciais e autentica o usuário
 
+    # Baixa o(s) arquivo(s) do local especificado e salva no diretório informado
     api.dataset_download_files(
         "nudratabbas/hospital-records-for-data-cleaning-medium",
         output_dir,
-        unzip=True
+        unzip=True # Se estiver zipado descompacta automaticamente
     )
 
     logging.info(f"Dados baixados em {output_dir}...")
