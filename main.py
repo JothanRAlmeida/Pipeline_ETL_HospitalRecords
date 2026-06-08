a1bf90d36efbda53b9a3d4563ac56ec67707a616
Jothan <jothanalmeida7@gmail.com>
2026-06-08 18:00:45 -0300

    docs: adiciona comentários detalhando as etapas na main

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from src.load_data import load_hospital_data
 from pathlib import Path
 import pandas as pd
 
+# Nome da tabela que será enviada ao banco de dados
 table_name = 'hospital_records'
 
+# Extração
 extract_hospital_data()
 
+# Transformação
 df = transform_data_hospital()
 
+# Carga
 load_hospital_data(table_name, df)
 
 print("Pipeline executado com sucesso!\n\n")
