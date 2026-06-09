bfdf36b7bf4a54a0a1face88099dcaab5cfb25c3
Jothan <jothanalmeida7@gmail.com>
2026-06-09 19:03:37 -0300

    fix: remove bibliotecas não utilizadas

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from src.extract_data import extract_hospital_data
 from src.transform_data import transform_data_hospital
 from src.load_data import load_hospital_data
-from pathlib import Path
-import pandas as pd
 
 # Nome da tabela que será enviada ao banco de dados
 table_name = 'hospital_records'
